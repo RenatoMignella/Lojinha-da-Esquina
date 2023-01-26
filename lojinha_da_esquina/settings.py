bae305e3a72e79f9e9fe49cbbee81937e52806e2
RenatoMignella <renatoferigato@gmail.com>
2023-01-26 21:34:56 +0000

    fix secret exposure

--- lojinha_da_esquina/settings.py
+++ lojinha_da_esquina/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3$4ya!f-a#h6p1s2dd=vd221(+@c-!e4bm90yw9*kxxq%04ecj'
+SECRET_KEY = 'django-insecure-3$4ya!f-a#h6p1s2dd=vd221(+@c-!e4bm90yw9*kxxq%04ecjR'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
