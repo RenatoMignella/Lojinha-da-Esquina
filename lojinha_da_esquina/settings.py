74a45286ad9cb1e04dea65b25e469104bf525d9b
RenatoMignella <renatoferigato@gmail.com>
2023-02-05 00:50:26 +0000

    remove secret key and debug set

--- lojinha_da_esquina/settings.py
+++ lojinha_da_esquina/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3$4ya!f-a#h6p1s2dd=vd221(+@c-!e4bm90yw9*kxxq%04ecjRR'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['lojinha-da-esquina.herokuapp.com', 'localhost']
 
